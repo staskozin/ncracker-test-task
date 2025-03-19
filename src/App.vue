787de03f17d0dfe7f9ce492cec2a6bddb7008c5e
Stanislav Kozin <stas@staskozin.ru>
2025-03-19 19:14:58 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <nav>
+      <RouterLink to="/">Главная</RouterLink>
+      <RouterLink to="/movie">Фильм</RouterLink>
+    </nav>
+  </header>
+
+  <RouterView />
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>Список фильмов</h1>
+</template>
