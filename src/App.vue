054a590c402c254e9de8b6ae1106034f7385e513
Stanislav Kozin <stas@staskozin.ru>
2025-03-24 18:31:27 +0300

    Add content container

--- src/App.vue
+++ src/App.vue
@@ -6,9 +6,18 @@ import PageHeader from './components/PageHeader.vue';
 <template>
   <PageHeader />
 
+  <div class="container">
     <nav>
       <RouterLink to="/">Главная</RouterLink>
       <RouterLink to="/movie">Фильм</RouterLink>
     </nav>
-  <RouterView />
+    <RouterView />
+  </div>
 </template>
+
+<style scoped lang="scss">
+.container {
+  max-width: 1400px;
+  margin: 0 auto;
+}
+</style>
